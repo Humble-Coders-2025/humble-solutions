5016fa89b46529dd5c0221b844ad7c1fa5d2b364
Nishesh <nsingla3_be23@thapar.edu>
2025-09-08 16:13:49 +0530

    prod fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/humble-solutions/', // ✅ must match repo name!
+  base: './', 
   plugins: [react(),tailwindcss()],
 })
